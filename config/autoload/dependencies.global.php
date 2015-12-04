97e5e2f3eb18373d0ac4d63beb1dab7a9a29ed47
Mark Pitman <mark.ptiman@learninga-z.com>
2015-12-04 14:51:57 -0500

    added cache to config

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -2,20 +2,19 @@
 
 return [
     'dependencies' => [
-        'invokables' => [
-            //App\Action\PingAction::class => App\Action\PingAction::class,
-        ],
         'factories' => [
             Teacher\Action\TeacherAction::class => Teacher\Factory\TeacherActionFactory::class,
             Teacher\Service\TeacherService::class => Teacher\Factory\TeacherServiceFactory::class,
 
-
-            //App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
             Zend\Expressive\Application::class => Zend\Expressive\Container\ApplicationFactory::class,
         ],
         'abstract_factories' => [
             Zend\Db\Adapter\AdapterAbstractServiceFactory::class,
-            //Zend\Cache\Service\StorageCacheAbstractServiceFactory::class
+            Zend\Cache\Service\StorageCacheAbstractServiceFactory::class
+        ],
+
+        'initializers' => [
+            Common\Cache\RedisCacheInitializer::class
         ],
     ]
 ];
